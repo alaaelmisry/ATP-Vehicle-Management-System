8c20b53e4f6fdeb736b1df4b8f541b301974e62c
alaaelmisry <3laaelmisry@gmail.com>
2026-07-12 15:02:31 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,22 +1,61 @@
-// ==========================
-// data.js
-// ==========================
+const vehicles = [];
 
-const vehicleData = [];
 
-for (let i = 1; i <= 100; i++) {
+// PRIVATE 30 مركبة
 
-    vehicleData.push({
+for(let i=1; i<=30; i++){
 
-        id: i,
+    vehicles.push({
 
-        number: "ATP-" + String(i).padStart(3, "0"),
+        id:i,
 
-        status: "green",   // green - red - yellow
+        number:"P-" + String(i).padStart(3,"0"),
 
-        driver: "",
+        type:"private",
 
-        notes: ""
+        status:"working"
+
+    });
+
+}
+
+
+
+
+// TRUCKS 35 شاحنة
+
+for(let i=1; i<=35; i++){
+
+    vehicles.push({
+
+        id:30+i,
+
+        number:"T-" + String(i).padStart(3,"0"),
+
+        type:"truck",
+
+        status:"working"
+
+    });
+
+}
+
+
+
+
+// EQUIPMENTS 35 معدة
+
+for(let i=1; i<=35; i++){
+
+    vehicles.push({
+
+        id:65+i,
+
+        number:"EQ-" + String(i).padStart(3,"0"),
+
+        type:"equipment",
+
+        status:"working"
 
     });
 
